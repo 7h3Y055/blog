b013161bc66e2c23140261f6d5868de4c3e385e2
7h3Y055 <youssefbouchmama18@outlook.com>
2026-03-14 00:09:28 +0000

    Quartz sync: Mar 14, 2026, 12:09 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "7h3's Notes",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
